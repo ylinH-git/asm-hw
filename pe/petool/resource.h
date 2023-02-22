c597a7bc9b74c4affa09066916fe7cbae30d19da
yellin <king22224444@163.com>
2023-02-23 07:44:55 +0800

    fix: petool

---
+++ pe/petool/resource.h
@@ -0,0 +1,26 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 petool.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PETOOL_DIALOG               102
+#define IDR_MAINFRAME                   128
+#define DLG_FILE_INFO                   130
+#define TREE_PE                         1000
+#define LIST_FILE_MORE_INFO             1009
+#define LIST_FILE_MAIN_INFO             1010
+#define IDC_MFCEDITBROWSE1              1010
+#define FILE_BROWSE                     1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
