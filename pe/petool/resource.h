4f63b175c470fcf171b94a9ac927d251025de3fd
yellin <king22224444@163.com>
2023-02-28 08:23:34 +0800

    feat: 节表注入窗口静态完成部分

--- pe/petool/resource.h
+++ pe/petool/resource.h
@@ -21,6 +21,7 @@
 #define DLG_IMPORT_DIRECTORY            138
 #define DLG_CHANGE_VAL                  139
 #define DLG_IMPORT_ADD                  140
+#define DLG_SECTION_INSERT              141
 #define TREE_PE                         1000
 #define LIST_FILE_MORE_INFO             1009
 #define LIST_FILE_MAIN_INFO             1010
@@ -46,16 +47,31 @@
 #define EDT_FUN                         1021
 #define EDT_NEW                         1022
 #define BTN_VA                          1022
+#define IDC_EDIT2                       1022
 #define BTN_RVA                         1023
 #define EDT_OLD2                        1023
 #define EDT_FIELD                       1023
+#define IDC_EDIT3                       1023
 #define BTN_FA                          1024
+#define IDC_EDIT4                       1024
+#define IDC_EDIT5                       1025
+#define IDC_EDIT6                       1026
 #define LIST_DLL                        1027
+#define IDC_EDIT8                       1027
 #define LIST_FUNC                       1028
+#define IDC_EDIT9                       1028
 #define BTN_DLL                         1029
+#define IDC_EDIT10                      1029
 #define BTN_FUNC                        1030
+#define IDC_EDIT11                      1030
 #define BTN_FUNC2                       1031
 #define BTN_INSERT                      1031
+#define IDC_EDIT12                      1031
+#define IDC_EDIT13                      1032
+#define IDC_BUTTON1                     1034
+#define IDC_EDIT7                       1035
+#define IDC_BUTTON2                     1036
+#define IDC_EDIT14                      1037
 
 // Next default values for new objects
 // 
@@ -63,7 +79,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        142
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_CONTROL_VALUE         1036
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
