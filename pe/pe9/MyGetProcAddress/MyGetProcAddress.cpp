4c6a7d0441909f0c868f7c800146a144b27c041e
Yellin <king22224444@163.com>
2023-03-07 09:32:00 +0800

    feat: 完成MyGetProcAddress

--- pe/pe9/MyGetProcAddress/MyGetProcAddress.cpp
+++ pe/pe9/MyGetProcAddress/MyGetProcAddress.cpp
@@ -5,9 +5,9 @@
 #include <string>
 #include <iostream>
 #include "MyGetProcAddress.h"
-
 using namespace std;
-
+typedef int (_stdcall * MyAddType)(int a, int b);
+typedef int (_stdcall * MyMsgBox)(DWORD, const char *, const char *, DWORD);
 FARPROC MyGetProcAddress(HMODULE hModule, LPSTR lpProcName)
 {
     IMAGE_DOS_HEADER dosHdr = {};
@@ -28,8 +28,8 @@ FARPROC MyGetProcAddress(HMODULE hModule, LPSTR lpProcName)
         int i = 0;
         while (i < currExp->NumberOfNames)
         {
-            nameAddr += i;
-            char* pName = (char*)(nameAddr + (DWORD)hModule);
+            DWORD* currAddr = nameAddr + i;
+            char* pName = (char*)(*currAddr + (DWORD)hModule);
             int nRet = strcmp(pName, lpProcName);
             if (nRet == 0)
             {
@@ -44,7 +44,22 @@ FARPROC MyGetProcAddress(HMODULE hModule, LPSTR lpProcName)
     {
         ordinal = (DWORD)lpProcName - currExp->Base;
     }
-
+    DWORD* funcAddrList = (DWORD*)(currExp->AddressOfFunctions + (DWORD)hModule);
+    DWORD currFuncAddr = *(funcAddrList + ordinal);
+    if (currFuncAddr >= pDataEntry->VirtualAddress && currFuncAddr <= (pDataEntry->VirtualAddress + pDataEntry->Size))
+    {
+        string sOtherDllFunc = (char*)(currFuncAddr + (DWORD)hModule);
+        int nPointPos = sOtherDllFunc.find_first_of('.');
+        string sDllName = sOtherDllFunc.substr(0, nPointPos);
+        string sFuncName = sOtherDllFunc.substr(nPointPos + 1);
+        HMODULE hDll = GetModuleHandle(sDllName.c_str());
+        return MyGetProcAddress(hDll, (LPSTR)sFuncName.c_str());
+    }
+    else
+    {
+        return (FARPROC) (currFuncAddr + (DWORD)hModule);
+    }
+  
 }
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
@@ -52,5 +67,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
+    HMODULE hModule = LoadLibrary("Test.Dll");
+    MyAddType pMyAdd = (MyAddType)MyGetProcAddress(hModule, (LPSTR)"MyAdd");
+    pMyAdd(1, 2);
+    MyMsgBox pMyMsg = (MyMsgBox)MyGetProcAddress(hModule, (LPSTR)"MyMsgBox");
+    pMyMsg(NULL, "TEST MSG", "TEST", MB_OK);
     return 0;
 }
